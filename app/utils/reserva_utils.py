d5c27563ae3a4f5c439fcd2a777a2f5599d0c1cc
Mateus3007 <mateus3007araujo@gmail.com>
2023-12-11 18:27:53 -0300

    Atualizado corrigido

--- app/utils/reserva_utils.py
+++ app/utils/reserva_utils.py
@@ -20,11 +20,21 @@ def atualizar_reserva():
     reserva = ReservasController.buscar_reserva(id_reserva)
     
     if reserva:
-        print("Deixar em branco para manter o mesmo valor.")
-        status_reserva = input(f"Status da reserva [{reserva.status_reserva}]: ") or reserva.status_reserva
-        data_reserva = input(f"Data da reserva [{reserva.data_reserva}]: ") or reserva.data_reserva
+        print("Deixe em branco para manter o mesmo valor.")
 
-        ReservasController.atualizar_reserva(id_reserva, status_reserva=status_reserva, data_reserva=data_reserva)
+        status_reserva = input(f"Status da reserva [{reserva.status_reserva}]: ")
+        status_reserva = status_reserva if status_reserva.strip() else reserva.status_reserva
+
+        data_reserva = input(f"Data da reserva [{reserva.data_reserva}]: ")
+        data_reserva = data_reserva if data_reserva.strip() else reserva.data_reserva
+
+        Livros_idLivros = input(f"ID do livro reservado [{reserva.Livros_idLivros}]: ")
+        Livros_idLivros = int(Livros_idLivros) if Livros_idLivros.strip() else reserva.Livros_idLivros
+
+        Usuarios_idUsuarios = input(f"ID do usuário que fez a reserva [{reserva.Usuarios_idUsuarios}]: ")
+        Usuarios_idUsuarios = int(Usuarios_idUsuarios) if Usuarios_idUsuarios.strip() else reserva.Usuarios_idUsuarios
+
+        ReservasController.atualizar_reserva(id_reserva, status_reserva=status_reserva, data_reserva=data_reserva, Livros_idLivros=Livros_idLivros, Usuarios_idUsuarios=Usuarios_idUsuarios)
         print("Reserva atualizada com sucesso!")
     else:
         print("Reserva não encontrada.")
@@ -43,4 +53,4 @@ def listar_reservas():
         print(
             f"ID: {reserva.idReservas}, Status: {reserva.status_reserva}, Data: {reserva.data_reserva}, Livro ID: {reserva.Livros_idLivros}, Usuário ID: {reserva.Usuarios_idUsuarios}"
         )
-remover_reserva()
+atualizar_reserva()
