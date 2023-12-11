e0110ccaa0378526897c6bcfd44e4451a9f2c6b3
Mateus3007 <mateus3007araujo@gmail.com>
2023-12-11 18:22:30 -0300

    atualizar, listar e remover

--- app/utils/reserva_utils.py
+++ app/utils/reserva_utils.py
@@ -23,12 +23,24 @@ def atualizar_reserva():
         print("Deixar em branco para manter o mesmo valor.")
         status_reserva = input(f"Status da reserva [{reserva.status_reserva}]: ") or reserva.status_reserva
         data_reserva = input(f"Data da reserva [{reserva.data_reserva}]: ") or reserva.data_reserva
-        Livros_idLivros = int(input(f"ID do livro reservado [{reserva.Livros_idLivros}]: ")) or reserva.Livros_idLivros
-        Usuarios_idUsuarios = int(input(f"ID do usuário que fez a reserva [{reserva.Usuarios_idUsuarios}]: ")) or reserva.Usuarios_idUsuarios
 
-        ReservasController.atualizar_reserva(id_reserva, status_reserva=status_reserva, data_reserva=data_reserva, Livros_idLivros=Livros_idLivros, Usuarios_idUsuarios=Usuarios_idUsuarios)
+        ReservasController.atualizar_reserva(id_reserva, status_reserva=status_reserva, data_reserva=data_reserva)
         print("Reserva atualizada com sucesso!")
     else:
         print("Reserva não encontrada.")
 
-atualizar_reserva()
+def remover_reserva():
+    print("\n--- Remover Reserva ---")
+    id_reserva = int(input("ID da reserva a ser removida: "))
+    ReservasController.deletar_reserva(id_reserva)
+    print("Reserva removida com sucesso.")
+
+
+def listar_reservas():
+    print("\n--- Lista de Reservas ---")
+    reservas = ReservasController.buscar_reservas()
+    for reserva in reservas:
+        print(
+            f"ID: {reserva.idReservas}, Status: {reserva.status_reserva}, Data: {reserva.data_reserva}, Livro ID: {reserva.Livros_idLivros}, Usuário ID: {reserva.Usuarios_idUsuarios}"
+        )
+remover_reserva()
