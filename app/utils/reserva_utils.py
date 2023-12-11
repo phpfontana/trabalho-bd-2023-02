a713d9ba51ee9781422f2ecb8cc100932e801555
Mateus3007 <mateus3007araujo@gmail.com>
2023-12-11 17:53:24 -0300

    .

--- app/utils/reserva_utils.py
+++ app/utils/reserva_utils.py
@@ -14,5 +14,21 @@ def cadastrar_reserva():
     else:
         print("Erro ao cadastrar reserva.")
 
+def atualizar_reserva():
+    print("\n--- Atualizar Reserva ---")
+    id_reserva = int(input("ID da reserva a ser atualizada: "))
+    reserva = ReservaController.buscar_reserva_por_id(id_reserva)
+    
+    if reserva:
+        print("Deixar em branco para manter o mesmo valor.")
+        status_reserva = input(f"Status da reserva [{reserva.status_reserva}]: ") or reserva.status_reserva
+        data_reserva = input(f"Data da reserva [{reserva.data_reserva}]: ") or reserva.data_reserva
+        Livros_idLivros = int(input(f"ID do livro reservado [{reserva.Livros_idLivros}]: ")) or reserva.Livros_idLivros
+        Usuarios_idUsuarios = int(input(f"ID do usuário que fez a reserva [{reserva.Usuarios_idUsuarios}]: ")) or reserva.Usuarios_idUsuarios
 
-cadastrar_reserva()
+        ReservasController.atualizar_reserva(id_reserva, status_reserva=status_reserva, data_reserva=data_reserva, Livros_idLivros=Livros_idLivros, Usuarios_idUsuarios=Usuarios_idUsuarios)
+        print("Reserva atualizada com sucesso!")
+    else:
+        print("Reserva não encontrada.")
+
+atualizar_reserva()
